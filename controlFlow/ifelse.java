5bfbc995c94e19e54952bbcfaac83b7a33f4be4b
Nyfong <litongfong12@gmail.com>
2024-10-30 19:17:09 +0700

    secound commit

--- controlFlow/ifelse.java
+++ controlFlow/ifelse.java
@@ -1,9 +1,16 @@
 package controlFlow;
-public class ifelse{
 
-    public  static void main(String[] args)
+public class Ifelse {
+    public static void main (String [] args)
     {
-        System.out.println("hello");
+        String weather = "normal";
+        String rainy = "Rainy";
+        if(weather == rainy)
+        {
+            System.out.println("Bring Umbrella");
+        }
+        else {
+            System.out.println("Keep going");
+        }
     }
-
 }
