e90aeae408652552f900fa53f66454d6e3fd219a
Nyfong <litongfong12@gmail.com>
2024-10-31 10:15:37 +0700

    switch

---
+++ controlFlow/Switch.java
@@ -0,0 +1,21 @@
+package controlFlow;
+
+public class Switch {
+    int globalNumber = 10;
+    public static void main (String[] args)
+    {
+       Switch  object = new Switch();
+        int option = 1;
+        switch(option)
+        {
+                 case 1 ->{
+                 System.out.println(object.globalNumber++); //10
+                 System.out.print(object.globalNumber); //11
+                    break;
+                 }
+                default -> {
+                    System.out.println("None of above");
+                }
+        }
+    }
+}
